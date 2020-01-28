481db1ab6c82c187c81ba124391a0609e0f19835
David Brown <dmlb2000@gmail.com>
2020-01-28 10:13:26 -0800

    Add Ansible to Travis CI (#21)
    This adds running Ansible to setup Travis CI environment for
    testing.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- test_files/archiveinterfacepreload.py
+++ test_files/archiveinterfacepreload.py
@@ -3,9 +3,8 @@
 """Preload the archive interface for testing."""
 import requests
 
-SESSION = requests.Session()
 for i in range(1100, 1110):
-    resp = SESSION.put(
+    resp = requests.put(
         'http://127.0.0.1:8080/{}'.format(i),
         data='The data for file {}.\n'.format(i)
     )
